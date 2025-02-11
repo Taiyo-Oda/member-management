f8b0b08ecafc0c7c9af621c3e9be856f4079a9c2
小田太陽 <noda0630@gmail.com>
2025-02-11 09:53:49 +0900

    共通のヘッダーコンポーネントを作成し、ベースとなるホーム画面を作成した

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -10,9 +10,9 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue'),
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/LoginView.vue'),
     },
   ],
 })
