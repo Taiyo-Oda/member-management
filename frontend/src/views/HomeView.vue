e871c41e020d8c681f1e456f970a2e0541fb143e
小田太陽 <noda0630@gmail.com>
2025-04-02 17:45:43 +0900

    APIのCORS設定を行い、フロントエンドと疎通できることを確認した

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,7 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import axios from 'axios'
+
+const message = ref(null)
+
+onMounted(async () => {
+  await getMessage()
+})
+
+async function getMessage() {
+  const response = await axios.get('http://localhost:8080/api/v1/hello')
+  message.value = response.data
+}
+</script>
 
 <template>
   <div>
-    <h1>Home</h1>
+    <h1>{{ message }}</h1>
   </div>
 </template>
